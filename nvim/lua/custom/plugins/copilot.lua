36b4438e0e407916b1d9005d48ad29280153c772
bst <blazejstangierski@gmail.com>
2024-09-16 12:21:55 +0200

    upd tmux config upd copilot maping - fix for md

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,5 +1,28 @@
 local M = {
   'github/copilot.vim',
+  config = function()
+    -- Disable Copilot's default <Tab> mapping
+    -- vim.g.copilot_no_tab_map = true
+
+    -- Create an autocmd group to manage mappings without duplication
+    local copilot_md_group = vim.api.nvim_create_augroup('CopilotMarkdownMapping', { clear = true })
+
+    -- Set up the autocmd for Markdown filetype
+    vim.api.nvim_create_autocmd('FileType', {
+      group = copilot_md_group,
+      pattern = 'markdown',
+      callback = function()
+        -- Map <C-J> to Copilot's accept function in insert mode
+        vim.api.nvim_buf_set_keymap(
+          0, -- Buffer number (current buffer)
+          'i', -- Insert mode
+          '<Home>', -- Key combination: Control + J
+          'copilot#Accept()', -- Command to execute
+          { expr = true, silent = true, noremap = true }
+        )
+      end,
+    })
+  end,
 }
 
 return M
