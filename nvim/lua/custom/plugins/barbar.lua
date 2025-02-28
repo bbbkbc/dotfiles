bdb82ecae93a495b975d098ae181a9379e8505e9
bst <blazejstangierski@gmail.com>
2025-02-28 10:18:39 +0100

    # adjust colors for gitsigns when using tokyo

--- nvim/lua/custom/plugins/barbar.lua
+++ nvim/lua/custom/plugins/barbar.lua
@@ -1,58 +1,62 @@
 local M = {
   'romgrk/barbar.nvim',
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
   dependencies = {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
 
 function M.init()
   vim.g.barbar_auto_setup = false
-  local keymap = vim.api.nvim_set_keymap
-  local keymap_opts = { noremap = true, silent = true }
-  keymap('n', '<A-,>', '<Cmd>BufferPrevious<CR>', keymap_opts)
-  keymap('n', '<A-.>', '<Cmd>BufferNext<CR>', keymap_opts)
-  keymap('n', '<A-c>', '<Cmd>BufferClose<CR>', keymap_opts)
+  local barbar = require 'barbar'
+  local opts = {
+    animation = true,
+    clickable = false,
+    icons = {
+      button = '',
+      gitsigns = {
+        added = { enabled = true, icon = '+' },
+        changed = { enabled = true, icon = '~' },
+        deleted = { enabled = true, icon = '-' },
+      },
+      diagnostics = {
+        [vim.diagnostic.severity.ERROR] = { enabled = true },
+        [vim.diagnostic.severity.WARN] = { enabled = true },
+        [vim.diagnostic.severity.INFO] = { enabled = true },
+        [vim.diagnostic.severity.HINT] = { enabled = true },
+      },
+      modified = { button = '●' },
+      pinned = { button = '', filename = true },
+    },
+    -- Sets the maximum padding width with which to surround each tab
+    maximum_padding = 1,
+    -- Sets the minimum padding width with which to surround each tab
+    minimum_padding = 1,
+    -- Sets the maximum buffer name length.
+    maximum_length = 30,
+    -- Sets the minimum buffer name length.
+    minimum_length = 0,
+    sidebar_filetypes = {
+      NvimTree = true,
+      -- Or, specify the text used for the offset:
+      undotree = {
+        text = 'undotree',
+        align = 'right', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+      },
+    },
+  }
+  barbar.setup(opts)
+  -- local keymap = vim.api.nvim_set_keymap
+  -- local keymap_opts = { noremap = true, silent = true }
+  -- keymap('n', '<A-,>', '<Cmd>BufferPrevious<CR>', keymap_opts)
+  -- keymap('n', '<A-.>', '<Cmd>BufferNext<CR>', keymap_opts)
 end
 
-M.opts = {
-  animation = true,
-  clickable = false,
-  icons = {
-    button = '',
-    gitsigns = {
-      added = { enabled = true, icon = '+' },
-      changed = { enabled = true, icon = '~' },
-      deleted = { enabled = true, icon = '-' },
-    },
-    modified = { button = '●' },
-    pinned = { button = '', filename = true },
-  },
-  -- Sets the maximum padding width with which to surround each tab
-  maximum_padding = 1,
-
-  -- Sets the minimum padding width with which to surround each tab
-  minimum_padding = 1,
-
-  -- Sets the maximum buffer name length.
-  maximum_length = 30,
-
-  -- Sets the minimum buffer name length.
-  minimum_length = 0,
-  sidebar_filetypes = {
-    -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-    NvimTree = true,
-    -- Or, specify the text used for the offset:
-    undotree = {
-      text = 'undotree',
-      align = 'right', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-    },
-    -- Or, specify the event which the sidebar executes when leaving:
-    ['neo-tree'] = { event = 'BufWipeout' },
-    -- Or, specify all three
-    Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
-  },
-}
+vim.cmd [[
+highlight BufferCurrentADDED guibg=#282a36 guifg=#9ece6a
+highlight BufferCurrentCHANGED guibg=#282a36 guifg=#7aa2f7
+highlight BufferCurrentDELETED guibg=#282a36 guifg=#f7768e
+]]
 
 return M
