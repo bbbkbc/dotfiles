67d8d5c43ccd1978c2b648930f4171b8f07111f8
bst <blazejstangierski@gmail.com>
2025-04-02 09:34:29 +0200

    # Lsp update

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -9,27 +9,12 @@ local M = { -- LSP Configuration & Plugins
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
     { 'folke/neodev.nvim', opts = {} },
+
     'hrsh7th/cmp-nvim-lsp',
   },
 }
 
 function M.config()
-  -- Set up formatting on save using isort and black
-  local function format_on_save()
-    vim.api.nvim_create_augroup('FormatOnSave', { clear = true })
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*.py',
-      group = 'FormatOnSave',
-      callback = function()
-        vim.cmd 'silent! !isort %'
-        vim.cmd 'silent! !black %'
-      end,
-    })
-  end
-
-  -- Initialize the formatting setup
-  format_on_save()
-
   vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
     callback = function(event)
@@ -38,8 +23,9 @@ function M.config()
       --
       -- In this case, we create a function that lets us more easily define mappings specific
       -- for LSP related items. It sets the mode, buffer and description for us each time.
-      local map = function(keys, func, desc)
-        vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
       end
 
       map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
@@ -49,31 +35,24 @@ function M.config()
       map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
       map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-      map('K', vim.lsp.buf.hover, 'Hover Documentation')
+      map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
       map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+      local function client_supports_method(client, method, bufnr)
+        if vim.fn.has 'nvim-0.11' == 1 then
+          return client.supports_method(method, bufnr)
+        else
+          return client.supports_method(method, { bufnr = bufnr })
+        end
+      end
 
       -- The following two autocommands are used to highlight references of the
       -- word under your cursor when your cursor rests there for a little while.
       --    See `:help CursorHold` for information about when this is executed
       --
-      -- -- When you move your cursor, the highlights will be cleared (the second autocommand).
-      -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-      -- if client and client.server_capabilities.documentHighlightProvider then
-      --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      --     buffer = event.buf,
-      --     callback = vim.lsp.buf.document_highlight,
-      --   })
-      --
-      --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      --     buffer = event.buf,
-      --     callback = vim.lsp.buf.clear_references,
-      --   })
-      -- end
-      --
       -- When you move your cursor, the highlights will be cleared (the second autocommand).
       local client = vim.lsp.get_client_by_id(event.data.client_id)
-      if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+      if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
         local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
           buffer = event.buf,
@@ -100,13 +79,42 @@ function M.config()
       -- code, if the language server you are using supports them
       --
       -- This may be unwanted, since they displace some of your code
-      if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+      if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
         map('<leader>th', function()
           vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
         end, '[T]oggle Inlay [H]ints')
       end
     end,
   })
+
+  -- Diagnostic Config
+  -- See :help vim.diagnostic.Opts
+  vim.diagnostic.config {
+    severity_sort = true,
+    float = { border = 'rounded', source = 'if_many' },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    signs = vim.g.have_nerd_font and {
+      text = {
+        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+        [vim.diagnostic.severity.WARN] = '󰀪 ',
+        [vim.diagnostic.severity.INFO] = '󰋽 ',
+        [vim.diagnostic.severity.HINT] = '󰌶 ',
+      },
+    } or {},
+    virtual_text = {
+      source = 'if_many',
+      spacing = 2,
+      format = function(diagnostic)
+        local diagnostic_message = {
+          [vim.diagnostic.severity.ERROR] = diagnostic.message,
+          [vim.diagnostic.severity.WARN] = diagnostic.message,
+          [vim.diagnostic.severity.INFO] = diagnostic.message,
+          [vim.diagnostic.severity.HINT] = diagnostic.message,
+        }
+        return diagnostic_message[diagnostic.severity]
+      end,
+    },
+  }
 
   -- LSP servers and clients are able to communicate to each other what features they support.
   --  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -115,17 +123,7 @@ function M.config()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
   -- Enable the following language servers
-  --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-  --
-  --  Add any additional override configuration in the following tables. Available keys are:
-  --  - cmd (table): Override the default command used to start the server
-  --  - filetypes (table): Override the default list of associated filetypes for the server
-  --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-  --  - settings (table): Override the default settings passed when initializing the server.
-  --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
   local servers = {
-    -- clangd = {},
-    -- gopls = {},
     rust_analyzer = {
       ['rust-analyzer'] = {
         diagnostics = {
@@ -136,27 +134,13 @@ function M.config()
         },
       },
     },
-    pyright = {
-      cmd = { 'pyright-langserver', '--stdio' },
-      settings = {
-        python = {
-          analysis = {
-            verboseOutput = true,
-            autoSearchPaths = true,
-            useLibraryCodeForTypes = true,
-            disableOrganizeImports = true,
-            -- diagnosticMode = 'workspace',
-          },
-        },
-      },
-    },
+    pyright = {},
     ruff = {
       init_options = {
         settings = {
           logLevel = 'debug',
         },
       },
-      -- cmd = { 'ruff', 'server', '--preview' },
     },
     jsonls = {},
     markdownlint = {},
@@ -177,8 +161,6 @@ function M.config()
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     'stylua', -- Used to format Lua code
-    'black',
-    'isort',
     'ruff',
     'pyright',
     'rust_analyzer',
@@ -187,6 +169,8 @@ function M.config()
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
   require('mason-lspconfig').setup {
+    ensure_installed = {},
+    automatic_installation = false,
     handlers = {
       function(server_name)
         local server = servers[server_name] or {}
